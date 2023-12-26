32ea22de326bd76143fbb99d9dec34f3b956fe9c
yet1009 <yeontae.y@ingstory.kr>
2023-12-26 18:22:56 +0900

    second

---
+++ src/components/sections/Map.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Calendar.module.scss';
+import Section from "@shared/Section";
+
+const cx = classNames.bind(styles);
+
+
+const Map = () => {
+    return (
+        <Section>
+
+        </Section>
+    );
+};
+
+export default Map;
